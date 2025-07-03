ae8632688df51b6f9cc878ce24aec204f9279e92
mathe-man <mathieumousset6@gmail.com>
2025-07-03 20:09:37 +0300

    Add doxygen comment in Component.h

--- src/Core/Component.h
+++ src/Core/Component.h
@@ -7,11 +7,22 @@ class GameObject;
 
 class Component {
 public:
+    /** @brief The GameObject this Component is attached to*/
     GameObject* owner;
+    /** @brief Is called when the owner of this component call Awake()*/
     virtual void Awake  ()  {}
+    /** @brief Is called when the owner of this component call Start()*/
     virtual void Start  ()  {}
+    /** @brief Is called when the owner of this component call Update()*/
     virtual void Update ()  {}
+    /** @brief Is called to draw to the scene.
+     * @remark Use Raylib drawing methods*/
     virtual void Draw   ()  {}
+    /** @brief Is called to draw this component in the inspector
+     * @remarks This is called in a ImGui frame, so use rlImGui or ImGui methods.
+     * @remarks The Editor already create an ImGui window using ImGui::Begin().
+     * @remarks Create a new ImGui window only if it's necessary.
+     */
     virtual void Inspect() = 0;
 
     virtual const char* GetName() const = 0;
